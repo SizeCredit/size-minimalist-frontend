0b724ce07adbcfb43674b3f0fedb3be247e2e5ca
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-10-04 11:00:04 -0300

    Add maxDays filter

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -33,6 +33,7 @@ const Charts = () => {
   const { tokens } = market
   const { price } = useContext(PriceContext)
   const [amount, setAmount] = useState(market.minimumCreditAmount)
+  const [maxDays, setMaxDays] = useState(365)
 
   const borrowOffers = filterOffers(tokens, allBorrowOffers, amount, false, price)
   const loanOffers = filterOffers(tokens, allLoanOffers, amount, true, price)
@@ -40,7 +41,7 @@ const Charts = () => {
   // Combine all unique tenors
   // const days = [...new Set([...borrowOffers.flatMap(o => o.curveRelativeTime.tenors), ...loanOffers.flatMap(o => o.curveRelativeTime.tenors)])].sort((a, b) => a - b)
   //   .map(e => e / 60 / 60 / 24)
-  const days = Array.from({ length: 365 }, (_, i) => i + 1)
+  const days = Array.from({ length: maxDays }, (_, i) => i + 1)
 
   // Prepare data for the chart
   const data = days.map(day => {
@@ -119,6 +120,10 @@ const Charts = () => {
       <div className='chart-amount'>
         <label>Amount ({(market.tokens.UnderlyingBorrowToken.symbol)})</label>
         <input type="text" value={amount} onChange={e => setAmount(Number(e.target.value))} />
+      </div>
+      <div className='chart-days'>
+        <label>Max Days</label>
+        <input type="text" value={maxDays} onChange={e => setMaxDays(Number(e.target.value))} />
       </div>
       <ResponsiveContainer width="100%" height={500}>
         <LineChart data={data} margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
