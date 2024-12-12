04765e3dd63cd58d8d88549ea969aea80c13ce3b
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-12-12 10:57:30 -0300

    Add input validation

--- src/components/Factory.tsx
+++ src/components/Factory.tsx
@@ -1,9 +1,11 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ConfigContext } from "../contexts/ConfigContext";
 import { PriceFeedParamsStruct } from "../typechain/SizeFactory";
+import { isAddress, parseUnits } from "viem";
 
 const Factory = () => {
   const { chain } = useContext(ConfigContext);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const [priceFeedParams, setPriceFeedParams] = useState<PriceFeedParamsStruct>(
     {
       uniswapV3Factory: chain.UniswapV3Factory,
@@ -28,9 +30,60 @@ const Factory = () => {
     });
   };
 
+  const formatInput = (value: string): string | undefined => {
+    value = value.trim();
+    value = value.replace(/_/g, "");
+    // address
+    if (isAddress(value)) {
+      return `"${value}"`;
+    }
+    // time
+    else if (value.includes("second")) {
+      return `"${Number(value.replace(/seconds?/g, ""))}"`;
+    } else if (value.includes("minute")) {
+      return `"${Number(value.replace(/minutes?/g, "")) * 60}"`;
+    } else if (value.includes("hour")) {
+      return `"${Number(value.replace(/hours?/g, "")) * 60 * 60}"`;
+    } else if (value.includes("day")) {
+      return `"${Number(value.replace(/days?/g, "")) * 60 * 60 * 24}"`;
+    }
+    // scientific notation
+    else if (value.includes("e")) {
+      const [mantissa, exponent] = value.split("e");
+      if (
+        !Number.isNaN(Number.parseFloat(mantissa)) &&
+        Number.isInteger(Number.parseFloat(exponent))
+      ) {
+        return `"${parseUnits(mantissa, Number(exponent))}"`;
+      } else {
+        return undefined;
+      }
+    }
+    // integer
+    else if (Number.isInteger(Number(value))) {
+      return `"${value}"`;
+    } else {
+      return undefined;
+    }
+  };
+
   const safeInput = `[${Object.values(priceFeedParams)
-    .map((value) => `"${value}"`)
+    .map((value) => value.toString())
+    .map(formatInput)
     .join(",")}]`;
+
+  const isValid = Object.values(priceFeedParams).map(
+    (value) => formatInput(value.toString()) !== undefined,
+  );
+
+  console.log(safeInput);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = "auto";
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
+  }, [safeInput]);
 
   return (
     <>
@@ -47,6 +100,9 @@ const Factory = () => {
               <tr
                 key={key}
                 className={index % 2 === 0 ? "even-row" : "odd-row"}
+                style={{
+                  backgroundColor: isValid[index] ? "white" : "lightpink",
+                }}
               >
                 <td>{key}</td>
                 <td>
@@ -76,15 +132,20 @@ const Factory = () => {
           <h5>Safe input</h5>
           <div className="flex-row safe-input-row">
             <textarea
+              ref={textAreaRef}
+              disabled={!isValid.every((isValid) => isValid)}
               style={{
                 whiteSpace: "pre-wrap",
                 fontFamily: "monospace",
+                backgroundColor: isValid.every((isValid) => isValid)
+                  ? "white"
+                  : "lightpink",
               }}
               value={safeInput}
               readOnly
-              rows={2}
             />
             <button
+              disabled={!isValid.every((isValid) => isValid)}
               title="Copy to clipboard"
               className="button"
               onClick={() => navigator.clipboard.writeText(safeInput)}
