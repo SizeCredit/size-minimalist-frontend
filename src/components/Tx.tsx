9a8c1916ba4d350c42729e55b1583458541f686f
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-12-13 16:59:59 -0300

    Add draft

--- src/components/Tx.tsx
+++ src/components/Tx.tsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createWalletClient } from "viem";
 import { custom } from "viem";
-import { sendTransaction } from "viem/actions";
+import { sendTransaction, readContract } from "viem/actions";
 import { useAccount } from "wagmi";
 import * as chains from "viem/chains";
 
 const Tx = () => {
+  const [safe, setSafe] = useState("");
   const [to, setTo] = useState("");
   const [amount, setAmount] = useState("");
   const [calldata, setCalldata] = useState("");
+  const [signature, setSignature] = useState("");
   const { address, chainId } = useAccount();
 
   const chain = Object.values(chains).find((c) => c.id === chainId);
 
+  const client = createWalletClient({
+    chain: chain,
+    transport: custom(window.ethereum),
+  });
+
   const sendTx = () => {
-    const client = createWalletClient({
-      chain: chain,
-      transport: custom(window.ethereum),
-    });
     sendTransaction(client, {
       to: to as `0x${string}`,
       value: BigInt(amount),
@@ -26,9 +29,40 @@ const Tx = () => {
     });
   };
 
+  const signSafeTx = async () => {
+    const safeAbi = [
+      "function getTransactionHash(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 _nonce) external view returns (bytes32)",
+    ];
+    const transactionHash = await readContract(client, {
+      address: safe as `0x${string}`,
+      abi: safeAbi,
+      functionName: "getTransactionHash",
+      args: [
+        // TODO fix args
+        to as `0x${string}`,
+        BigInt(amount),
+        calldata as `0x${string}`,
+        0,
+        0,
+        0,
+        address as `0x${string}`,
+        address as `0x${string}`,
+        0,
+      ],
+    });
+  };
+
   return (
     <>
       <div className="tx-container">
+        <div className="tx-input">
+          <label>Safe</label>
+          <input
+            type="text"
+            value={safe}
+            onChange={(e) => setSafe(e.target.value)}
+          />
+        </div>
         <div className="tx-grid">
           <div className="tx-input">
             <label>To</label>
@@ -55,9 +89,15 @@ const Tx = () => {
             />
           </div>
         </div>
+        <div className="tx-signature">
+          <textarea value={signature} readOnly />
+        </div>
         <div className="tx-button">
           <button className="action-button" onClick={() => sendTx()}>
             Send
+          </button>
+          <button className="action-button" onClick={() => signSafeTx()}>
+            Sign Safe Tx
           </button>
         </div>
         <div className="disclaimers">
