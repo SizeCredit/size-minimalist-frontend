91cec1c3e5ebe147a5fc2b5cacd786206a0b8cb0
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-01-12 19:03:43 -0300

    Remove useEffect

--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { LimitOrdersContext } from "../contexts/LimitOrdersContext";
 import {
   LineChart,
