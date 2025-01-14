6fbeaee0d5a800b5eadcecba2df38f10ab0d8af7
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-01-14 11:02:55 -0300

    Minimal changes

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,16 +1,19 @@
+import { createPublicClient } from "viem";
 import { http, createConfig } from "wagmi";
 import { base, baseSepolia, sepolia, mainnet } from "wagmi/chains";
 import { injected, walletConnect } from "wagmi/connectors";
 
-const RPC_URLS = {
+export const RPC_URLS = {
   [base.id]: "https://base-mainnet.g.alchemy.com/v2/",
   [baseSepolia.id]: "https://base-sepolia.g.alchemy.com/v2/",
   [sepolia.id]: "https://eth-sepolia.g.alchemy.com/v2/",
   [mainnet.id]: "https://eth-mainnet.g.alchemy.com/v2/",
 };
 
+export const chains = [base, baseSepolia, sepolia, mainnet] as const;
+
 export const config = createConfig({
-  chains: [base, baseSepolia, sepolia, mainnet],
+  chains: chains,
   connectors: [
     injected(),
     walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
@@ -30,6 +33,19 @@ export const config = createConfig({
     ),
   },
 });
+
+export const publicClients = Object.keys(RPC_URLS).map((chainId) =>
+  ({
+    [chainId]: createPublicClient({
+      chain: chains.find((chain) => chain.id === Number(chainId)),
+      transport: http(
+        `${RPC_URLS[chainId as unknown as keyof typeof RPC_URLS]}${
+          import.meta.env.VITE_ALCHEMY_API_KEY
+        }`,
+      ),
+    }),
+  }),
+).reduce((acc, curr) => ({ ...acc, ...curr }), {});
 
 declare module "wagmi" {
   interface Register {
